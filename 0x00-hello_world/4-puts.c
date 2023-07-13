6777d3109969b2202a9953904fa01eed0b66769d
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-13 23:47:22 +0200

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /* 
- * main -
- * Return 0
+ * main - using the put function to print a statement
+ * Description: using main funciton
+ * Return: 0
  */
 
 int manin(void)
 {
-	puts(\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
