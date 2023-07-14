639ad21c3952514fa93254e37968bb76b5633cc0
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-14 14:07:22 +0200

    size_of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ print_size_of_type(void)
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of a float: %d byte(s)\n",sizeof(float));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 }
 int main(void)
 {
