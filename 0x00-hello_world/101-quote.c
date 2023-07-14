d5d1c96c3451667e7f34cd5961284c34aa4513eb
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-14 16:07:14 +0200

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	int i = STDERR_FILENO;
-
-	write(i,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen(msg));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
