2e83c139c8741b3e560dc5327c75ee7445bff863
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-14 13:24:21 +0200

    hello_printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - entry point to print a statement
+ * 
  * Description: using the printf function to return a statement
  * Return: 0
  */
 int main(void)
 {
-	printf(\"with proper grammar, but the outcome is a piece of art,\");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
