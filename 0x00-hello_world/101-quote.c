99609ee61ab1e918ac0d58c3eb05636e66fbd8c3
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-14 15:58:12 +0200

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point to print a qoute
+ * Description: using <unistd.h>, and write()
+ * Return: Always return 1 (successful)
+ */
+int main(void)
+{
+	int i = STDERR_FILEENO;
+	char msg = "and that piece of art is useful" - Dora Korpar, 2015-10-19;
+
+	write(i, msg, strlen(msg));
+	return (1);
+}
