38fd4a001a52c90e5b23f0aa4895dd7810013d9f
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-16 00:48:06 +0200

    comma_separated_numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:'printing out single digit-numbers'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	char com = ',';
+	char spac = ' ';
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(com);
+		putchar(spac);
+	}
+	putchar('\n');
+	return (0);
+}
