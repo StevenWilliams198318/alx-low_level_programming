ab394272a6a8b1c45a0b856805afc3610df497e0
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-16 01:18:11 +0200

    three_digit_combs

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'printing two digit numbers 01 - 99'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 0;
+	/* betty again*/
+	while (n <= 99)
+	{
+		printf("%03d", n);
+		if (n != 99)
+		{
+			printf(", ");
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
