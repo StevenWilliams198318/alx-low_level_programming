59ce80a3ab3d1ee7212eef6e9c5d3c5e7aa9645f
stevenwilliams198318 <stevenwilliams198318@gmail.com>
2023-07-16 00:04:09 +0200

    0x00-base_16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'printing base 16 number in lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 0x30;
+
+	while (n <= 0x39)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
